ef18631f9ec25e3cd4f7033c50b02aa2602c57dd
guilhermetvrs <guilhermetvrsjp@gmail.com>
2020-09-11 10:48:47 -0300

    adicionei tudo ai

---
+++ questao1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "no mundo animal,o mais forte é quem tem o chifre maior,agradeça a sua namorada por te tornar invencivel"
---
+++ questao3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dir1=1
+dir2=2
+dir3=3
+
+echo "diretorio 1:" $(ls $dir1)
+echo "diretorio 2:" $(ls $dir2)
+echo "diretorio 3:" $(ls $dir3)
