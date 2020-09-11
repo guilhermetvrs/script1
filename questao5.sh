ef79f1c3954720cd62919a31867c1e2248947e99
root <root@debian.root>
2020-09-11 16:24:02 -0300

    add novos arquivos

--- questao5.sh
+++ questao5.sh
@@ -5,5 +5,10 @@ echo $data
 
 $(mkdir /bin/${data})
 
-$(cp -r * /bin/${data})
+$(cp -r * /bin/${data}) && echo "concluido a criação"
 
+echo"questão 6,compactando"
+
+$(tar -czf data.tar.gz /bin/${data}) && echo "compactado"
+$(rm -r /bin/${data}) && echo "removido"
+$(cp /bin/data.tar.gz /root/script1) && echo "copiado"
